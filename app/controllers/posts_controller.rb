c84cf4760780e3dee60bf32a4e330063d989f97f
248 <aestivalis01+github@gmail.com>
2013-08-12 13:08:26 +0900

    取り敢えずリモートに置く

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,34 @@
+class PostsController < ApplicationController
+	def index
+		@posts = Post.all;
+	end
+
+	def show
+		@post = Post.find(params[:id]);
+	end
+
+	def new
+		@post = Post.new;
+	end
+
+	def create
+		@post = Post.new(post_params)
+		if @post.save
+			redirect_to posts_path
+		else
+			render action: 'new'
+		end
+	end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '56a0cfd86ab761323ad361547b4b891a6edd759b211664ee484c8d2f0881283a3a692ec907034831c1e76169aec7a0490738fb3c44f9c8536d29f9fcd6b6cf0d'
