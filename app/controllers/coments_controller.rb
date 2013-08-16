2917988d5aba62f39e5cfc88d65dbe0d7fae87c4
248 <aestivalis01+github@gmail.com>
2013-08-16 22:35:06 +0900

    Routing Error出るけど取り敢えずコミット

--- app/controllers/coments_controller.rb
+++ app/controllers/coments_controller.rb
@@ -1,2 +1,20 @@
 class ComentsController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = Post.find(params[:post_id]).comments.create(params[:comment])
+    redirect_to post_path(@post)
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def comment_params
+      params.require(:comment).permit(:commenter, :body, :post_id)
+    end
+
 end
